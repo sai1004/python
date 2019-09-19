76a3bcf840fb6cec03da35b0d376bcd285c0e781
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-19 16:04:42 +0530

    Create statements.py

---
+++ statements.py
@@ -0,0 +1,84 @@
+# <=============================== if statements==========================>
+
+# conditional tests
+x = 42
+
+x == 42    # equals
+
+x != 42   # not equal
+
+x > 42   # greater than
+
+x >= 42  # or equal to
+
+x < 42  # less than
+
+x <= 42  # or equal to
+
+names = ['james', 'bond', 'alex']
+
+if 'alex' in names:
+    print(names)
+    print('hello {} you are warned dont not repeate again!'.format('alex'))
+
+if 'sai' not in names:
+    print(names)
+    print('Sorry please wait for {} to Come in'.format('sai'))
+
+game_active = True
+
+can_edit = False
+
+
+age = 5
+if age < 4:
+    ticketPrice = 0
+    print(" Ticket For Age Less Than 4:", ticketPrice)
+elif age < 18:
+    ticketPrice = 10
+    print(" Ticket For Age Less Than 18: {}".format(ticketPrice))
+
+else:
+    ticketPrice = 15
+    print(" Ticket For Age Less Than 18: {}".format(ticketPrice))
+
+
+# <======================= Dicitionaries ===================>
+
+alien = { 'color':'green', 'points': 5 }
+
+# assigining a value
+
+print("The alien's Color is " + alien['color'])
+
+# adding new key-value pair
+
+alien['from'] = 'UFO'
+
+print(alien)
+
+
+# <-====================== user Input ====================>
+
+name = input('Whats Your Name?')
+
+print('Hello,' + name) # names in str format
+
+pi = input('What is the value of PI ?')
+pi = float(pi)
+if pi != 3.14159265:
+    print('wrong')
+
+#<========================= looping through all key-value pairs ======================>
+
+fav_nums = { 'eric': 17, 'dona': 4 }
+
+for name, number in fav_nums.items():
+    print( name + ' loves ' + str(number))
+
+# looping through all the values
+
+fav_nums = { 'eric': 17, 'dona': 4 }
+
+for number in fav_nums.values():
+    print( str(number) + ' is a fav')
