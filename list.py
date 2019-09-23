8d44a7c2767c0835b185fca4d3405bb976d2374d
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-23 07:56:43 +0530

    Update list.py

--- list.py
+++ list.py
@@ -41,3 +41,35 @@ finishers = ['sam','bob','amanda','linda']
 firstTwo = finishers[:2]
 
 print(firstTwo)
+
+
+# in python strings and numbers are imutable  that the value of the obj or var cannot be changed in mem location
+
+a = 2 # allocating the memory
+print(hex(id(a)))
+a = 30
+print(hex(id(a))) #to print the loc of mem allocation
+
+
+# in python lists are mutable 
+
+price = [ 10, 20, 30, 40, 50]
+
+print(price[-1])
+print(price[3])
+print(price[0:2])
+price.append(33)
+print(price)
+print(price.count(60)) # returns true or false if value exists returns 1(true)
+
+#changing the value of list
+price[0] = 60
+print(price)
+
+list1 = [ 22, 5, True , 78 , [20, 99, 'a']]
+print(list1[-1][1])
+
+len(list1)
+print(max(list1))
+print(min(list1))
+
