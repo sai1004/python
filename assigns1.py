ae2ab2e60a1f49d3bf917b0ccd7a17c11f664648
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-26 23:25:10 +0530

    Create assigns1.py

---
+++ assigns1.py
@@ -0,0 +1,60 @@
+# prob two 
+str1 = 'hello'
+str2 = ','
+str3 = 'world'
+print(str1[-1:])
+
+ 
+
+# prob three
+
+# What arithmetic operators can be used with strings?
+
+# prob 4 done
+print('new' 'line')
+
+# prob 5
+
+user = int(input(' '))
+
+# user.split(' ')
+
+print(user)
+
+furit = print('Enter your fav furit:')
+
+furit *= 3
+print(furit)
+
+
+
+user = input(" ")
+
+users = user.split(' ')
+
+for x in users:
+        print(x)
+
+user = input("Enter full name")
+
+print(user.split()[0])
+print(user.split()[1])
+
+print(user)
+
+name  =input("Enter Your Full Name:")
+names =name.replace(' ', '\n')
+print(names)
+
+fruit = input('Enter your fav furit:')
+
+fruit *= 3
+
+loo = fruit.index('apple')
+
+for x in loo:
+	print(x)
+
+print('new' 'line')
+
+
