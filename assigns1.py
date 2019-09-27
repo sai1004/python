268f8b9799b0eae0537d8dbe1bc18c393d614d6f
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-27 07:47:53 +0530

    Update assigns1.py

--- assigns1.py
+++ assigns1.py
@@ -1,60 +1,66 @@
-# prob two 
-str1 = 'hello'
-str2 = ','
-str3 = 'world'
-print(str1[-1:])
+Q1) What will be the output of the following statement?
+>>>"a"+"bc"
+
+output: abc
+
+Q2) What will be the output of the following Python code?
+
+>>> str1 = 'hello'
+>>> str2 = ','
+>>> str3 = 'world'
+>>> str1[-1:]
+
+output:
+hello
+,
+world
+o
 
  
+Q4)Write a program that accepts user's full name e.g. Albert Watson.
+Print first name in the first line and last name in the second line?
+#way one
+user = input("Enter first name and second name: ")
 
-# prob three
+users = user.split(' ')
+for x in users:
+    print(x)
 
-# What arithmetic operators can be used with strings?
+#way two
+user = input("Enter full name")
+print(user.split()[0])
+print(user.split()[1])
 
-# prob 4 done
-print('new' 'line')
-
-# prob 5
-
-user = int(input(' '))
-
-# user.split(' ')
-
+#way three
 print(user)
+name  =input("Enter Your Full Name:")
+names =name.replace(' ', '\n')
+print(names)
+
+output:
+	Albert
+	Watson
+
+Q5) Request user to input any fruit name. Print that fruit name thrice?
 
 furit = print('Enter your fav furit:')
 
 furit *= 3
 print(furit)
 
+fruitName = input("Enter any fruit name: ")
+
+fruit = (fruitName + " ") * 3
+
+# fruit = (fruitName + "\n") * 3 this breaks the line
+print(fruit)
 
 
-user = input(" ")
+output: apple apple apple
+ 
 
-users = user.split(' ')
 
-for x in users:
-        print(x)
+ 
 
-user = input("Enter full name")
-
-print(user.split()[0])
-print(user.split()[1])
-
-print(user)
-
-name  =input("Enter Your Full Name:")
-names =name.replace(' ', '\n')
-print(names)
-
-fruit = input('Enter your fav furit:')
-
-fruit *= 3
-
-loo = fruit.index('apple')
-
-for x in loo:
-	print(x)
-
-print('new' 'line')
-
+ 
 
