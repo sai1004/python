623f0fac047ec8f2bd9b13caa67206ef0a9d74ee
sai1004 <saikiran.bavandla@gmail.com>
2019-09-30 15:32:43 +0530

    commit

---
+++ Assignments/assconditional.py
@@ -0,0 +1,36 @@
+""" without logical operator """
+
+n1 = int(input('Please Eneter a Number: '))
+n2 = int(input('Please Eneter a Number: '))
+n3 = int(input('Please Eneter a Number: '))
+
+arr = n1, n2, n3
+parse = list(arr)
+print(max(parse))
+print(min(parse))
+
+if (n1 >= n2 ) and ( n1 >= n3):
+    max = n1
+elif (n2 >= n1 ) and (n2 >= n3):
+    max = n2
+print("The Largest Number between Three Numbers is", max)
+# 3) Take 10 inputs from the user as Number
+
+userInput = input('Please Enter the Numbers :')
+
+parse = list(userInput)
+
+sum = sum(parse)
+print(" Your Total out of 600 is: " + str(sum))
+avg = sum * 100 / 600
+
+print(' Your Avg Score out of 100%: ' + str(avg)+'%')
+
+if avg >= 90:
+    print(' The Perfomance is very good keep it up')
+
+elif avg >= 60:
+    print(' The Perfomance is good')
+
+else:
+    print(' The Perfomance is bad, You Need To Try Hard ')
