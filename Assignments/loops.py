2ad02399157b1ab9f80c489edc84d0adda1fee45
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-01 08:46:21 +0530

    Update loops.py

--- Assignments/loops.py
+++ Assignments/loops.py
@@ -2,12 +2,20 @@
 
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+
+sumOfList1 = sum(list1)
+
+avgList1 = (sumOfList1 * 20) / 100
+
+print(avgList1)
+
+
 # add all even from a list , print sum of all even numbers
 
 evenList = []
 
 for i in list1:
-    if i % 2 == 0:
+    if i % 2 == 0:    # reminder of item (i) is equals to 0 then print value of item
         evenList.append(i)
 sumEven = sum(evenList)
 print(sumEven)
@@ -17,7 +25,7 @@ print(sumEven)
 oddList = []
 
 for i in list1:
-    if i % 2 == 1:
+    if i % 2 == 1:   # reminder of item (i) is equals to 1 then print value of item
         oddList.append(i)
 sumOdd = sum(oddList)
 print(sumOdd)
