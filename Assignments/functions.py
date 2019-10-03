f1d333ddff640397aa45bc3d47720f819cbc59ee
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-03 22:32:25 +0530

    Create functions.py

---
+++ Assignments/functions.py
@@ -0,0 +1,5 @@
+#Problem 6: write a program that extracts only +ive numbers from a list.
+#Example:
+#numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+#Result: [34.6, 44.9, 68.3, 44.6, 12.7]
+
