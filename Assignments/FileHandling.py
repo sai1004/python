da66b2cdcd0fff9e7bece36f773153309b1eec4d
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-08 18:19:18 +0530

    Update FileHandling.py

--- Assignments/FileHandling.py
+++ Assignments/FileHandling.py
@@ -15,3 +15,18 @@ else:
     
     
     
+""" 2. Count how many times that The Particular word is in the file.. """
+word = open('demo.txt','r')
+
+#chunks The para/sentence into words and stores as elements in list format
+search = word.read().split(' ')
+
+#Taking User input
+userInput = input('please Enter The word you Want to Count in File?')
+
+# Using Membership Operator to check whether The user Input is in file
+if userInput in search:
+    count = search.count(userInput)
+    print('The Word You Entered Is: ({}) is {} times in File'.format(userInput,count))
+else:
+    print('word does not exists in File')
