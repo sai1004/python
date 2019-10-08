dcf4c797e40fd4c45726563e5e98acfccbfad3bd
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-08 17:23:58 +0530

    Create FileHandling.py

---
+++ chapter2/FileHandling.py
@@ -0,0 +1,31 @@
+""" File Open Write (Over Writes) Mode """
+
+fw = open('demo.txt', 'w')
+
+print(fw.write('hey hi'))
+
+fw.close()
+
+""" File Open Read Mode """
+fr = open('demo.txt', 'r')
+
+print(fr.read())
+print(fr.tell()) # tells the current pointer position of element in file
+print(fr.seek(0))
+print(fr.read())
+
+fr = open('demo.txt', 'r')
+print(fr.read())
+
+fr.close()
+
+""" File Open Append (Update) Mode """
+
+fa = open('demo.txt','a+')
+fa.write('this line is appended')
+print(fa.read()) #returns the empty string
+print(fa.tell())
+print(fa.seek(0))
+print(fa.read())
+
+
