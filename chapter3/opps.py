59ae5ab00cc6ad8a7238e8366ad4510fe5d791b4
saikiran <35389534+sai1004@users.noreply.github.com>
2019-11-30 19:08:21 +0530

    Create opps.py

---
+++ chapter3/opps.py
@@ -0,0 +1,22 @@
+
+''' inheritence '''
+
+    class Animal:  
+        def speak(self):  
+            print("Animal Speaking")  
+    #child class Dog inherits the base class Animal  
+    class Dog(Animal):  
+        def bark(self):  
+            print("dog barking")  
+    d = Dog()  
+    d.bark()  
+    d.speak()  
+    
+    
+    '''
+    o/p:
+    
+    dog barking
+    Animal Speaking
+
+    '''
