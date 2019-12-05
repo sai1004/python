e02be6f05d705b8134ec3a774aa64c4f9a4f5879
saikiran <35389534+sai1004@users.noreply.github.com>
2019-12-05 22:36:04 +0530

    Update functions.py

--- Assignments/functions.py
+++ Assignments/functions.py
@@ -20,3 +20,18 @@ for j in numbers:
 
 # admin = input("Enter the Elements: ")
 # list(admin)  # converts tupple to list as array
+
+
+
+# getMissingNo takes list as argument 
+def getMissingNo(A): 
+    n = len(A) 
+    total = (n + 1)*(n + 2)/2
+    sum_of_A = sum(A) 
+    return total - sum_of_A 
+
+# Driver program to test above function 
+A = [1, 2, 4, 5, 6] 
+miss = getMissingNo(A) 
+print(miss) 
+# This code is contributed by Pratik Chhajer
