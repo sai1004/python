c19741a43ef067da994f8742438e2c5c2cde1e64
saikiran <35389534+sai1004@users.noreply.github.com>
2019-12-07 16:00:52 +0530

    Update dictionary.py

--- chapter1/dictionary.py
+++ chapter1/dictionary.py
@@ -1,3 +1,19 @@
+
+price = { " apple" : 20, "biscuits": 50, "kiwi" : 25}
+
+Shopping = [ int(input('please select items you want to buy:' " " ) )]
+
+total = 0.00
+
+for item in shopping:
+      total ±= price(item)
+     
+print(total) 
+
+
+# ==========================
+
+
 dict1 = {1:2,3:4}
 for k,v in dict1.items():
     print(v)
