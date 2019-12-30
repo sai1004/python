fcd1b2f86b29eb6dc3020fd00720493b147c10a4
sai1004 <saikiran.bavandla@gmail.com>
2019-12-30 19:43:11 +0530

    commit

--- crypto.py
+++ crypto.py
@@ -1,118 +1,73 @@
-#!/usr/bin/python3
-
-from crypto import Random
-from crypto.cipher import AES
-import os
-import os.path
-from os import listdir
-from os.path import isfile, join
-import time
+#!/usr/bin/python
 
 
-class Encryptor:
-    def __init__(self, key):
-        self.key = key
+from socket import *
 
-    def pad(self, s):
-        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+import optparse
 
-    def encrypt(self, message, key, key_size=256):
-        message = self.pad(message)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        return iv + cipher.encrypt(message)
+from threading import *
 
-    def encrypt_file(self, file_name):
-        with open(file_name, 'rb') as fo:
-            plaintext = fo.read()
-        enc = self.encrypt(plaintext, self.key)
-        with open(file_name + ".enc", 'wb') as fo:
-            fo.write(enc)
-        os.remove(file_name)
-
-    def decrypt(self, ciphertext, key):
-        iv = ciphertext[:AES.block_size]
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-        return plaintext.rstrip(b"\0")
-
-    def decrypt_file(self, file_name):
-        with open(file_name, 'rb') as fo:
-            ciphertext = fo.read()
-        dec = self.decrypt(ciphertext, self.key)
-        with open(file_name[:-4], 'wb') as fo:
-            fo.write(dec)
-        os.remove(file_name)
-
-    def getAllFiles(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dirs = []
-        for dirName, subdirList, fileList in os.walk(dir_path):
-            for fname in fileList:
-                if (fname != 'script.py' and fname != 'data.txt.enc'):
-                    dirs.append(dirName + "\\" + fname)
-        return dirs
-
-    def encrypt_all_files(self):
-        dirs = self.getAllFiles()
-        for file_name in dirs:
-            self.encrypt_file(file_name)
-
-    def decrypt_all_files(self):
-        dirs = self.getAllFiles()
-        for file_name in dirs:
-            self.decrypt_file(file_name)
+from termcolor import colored
 
 
-key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
-enc = Encryptor(key)
+""" This is 3 function"""
+
+def connScan(tgtHost, tgtPort):
+    try:
+        sock = socket(AF_INET,SOCK_STREAM)
+        sock.connect((tgtHost,tgtPort))
+        print(" [+] port {} tcp is Open".format(tgtPort))
+
+    except:
+        print(" [-] port {} tcp is Closed".format(tgtPort))
+
+    finally:
+        sock.close()
+
+""" This is 2 function"""
 
 
-def clear(): return os.system('cls')
+def portScan(tgtHost, tgtPorts):
+  
+    try:
+        tgtIP = gethostbyname(tgtHost)
+
+    except:
+        print("Unknown Host {} ".format(tgtHost))
 
 
-if os.path.isfile('data.txt.enc'):
-    while True:
-        password = str(input("Enter password: "))
-        enc.decrypt_file("data.txt.enc")
-        p = ''
-        with open("data.txt", "r") as f:
-            p = f.readlines()
-        if p[0] == password:
-            enc.encrypt_file("data.txt")
-            break
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print(" [+] Scan Results For " + tgtName[0])
+    except:
+        print(" [+] Scan Results For " + tgtIP)
+    setdefaulttimeout(1)
 
-    while True:
-        clear()
-        choice = int(input(
-            "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
-        clear()
-        if choice == 1:
-            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
-        elif choice == 2:
-            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
-        elif choice == 3:
-            enc.encrypt_all_files()
-        elif choice == 4:
-            enc.decrypt_all_files()
-        elif choice == 5:
-            exit()
-        else:
-            print("Please select a valid option!")
+    for tgtPort in tgtPorts:
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
 
-else:
-    while True:
-        clear()
-        password = str(
-            input("Setting up stuff. Enter a password that will be used for decryption: "))
-        repassword = str(input("Confirm password: "))
-        if password == repassword:
-            break
-        else:
-            print("Passwords Mismatched!")
-    f = open("data.txt", "w+")
-    f.write(password)
-    f.close()
-    enc.encrypt_file("data.txt")
-    print("Please restart the program to complete the setup")
-    time.sleep(15)
+""" This is main function"""
+
+def main():
+
+    parser = optparse.OptionParser('Usage Of Program: ' + '-H <traget host> -p <target port>')
+
+    parser.add_option('-H', dest='tgtHost', type='string',help='specify target host')
+
+    parser.add_option('-p', dest='tgtPort', type='string',help='specify target ports seperated by comma')
+
+    (options, args) = parser.parse_args()
+
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(',')
+
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print(parser.usage)
+        exit(0)
+
+    portScan(tgtHost,tgtPorts)
+
+
+if __name__ == '__main__':
+    main()
