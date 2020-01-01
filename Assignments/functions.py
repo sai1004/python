4e56ce2b49cc4b512f792e193ee3c4ddd80170d7
saikiran <35389534+sai1004@users.noreply.github.com>
2020-01-01 16:10:19 +0530

    Update functions.py

--- Assignments/functions.py
+++ Assignments/functions.py
@@ -75,3 +75,28 @@ def decrypt():
 
 
 print(decrypt())
+
+
+# Install following two packages before executing the code below
+
+# pip install beautifulsoup4  
+
+# pip install google
+
+try: 
+
+  from googlesearch import search 
+
+except ImportError: 
+
+  print("No module named 'google' found") 
+
+# to search 
+
+query = "python for engineers Blog"
+
+for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
+
+  print(j) 
+
+
