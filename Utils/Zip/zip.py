a07a66096eb6802937543c008ba6c82f1be77d89
sai1004 <saikiran.bavandla@gmail.com>
2020-01-23 17:55:47 +0530

    commit

---
+++ Utils/Zip/zip.py
@@ -0,0 +1,62 @@
+"""
+how to compress and decompress files, folders and symbolic links in Python 
+using gzip compression in tarfile built-in module.
+
++=======================================+
+| packages Required:                    |
+|    pip3 install tqdm                  |
+|                                       |
++=======================================+
+
+
+"""
+
+import tarfile
+from tqdm import tqdm
+
+
+def compress(tar_file, members):
+    """
+    Adds files (`members`) to a tar_file and compress it
+    """
+    # open file for gzip compressed writing
+    tar = tarfile.open(tar_file, mode="w:gz")
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        # add file/folder/link to the tar file (compress)
+        tar.add(member)
+        # set the progress description of the progress bar
+        progress.set_description(f"Compressing {member}")
+    # close the file
+    tar.close()
+
+
+compress("compressed.tar.gz", ["demo.txt"])
+
+# compress("compressed.tar.gz", ["demo.txt","folder"]) # can add multipule files/folders in 2nd param
+
+
+def decompress(tar_file, path, members=None):
+    """
+    Extracts `tar_file` and puts the `members` to `path`.
+    If members is None, all members on `tar_file` will be extracted.
+    """
+    tar = tarfile.open(tar_file, mode="r:gz")
+    if members is None:
+        members = tar.getmembers()
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        tar.extract(member, path=path)
+        # set the progress description of the progress bar
+        progress.set_description(f"Extracting {member.name}")
+    # or use this
+    # tar.extractall(members=members, path=path)
+    # close the file
+    tar.close()
+
+
+decompress("compressed.tar.gz", "extracted")
