b9bae57bfed99b94a600c653a10bf186f89a9da5
sai1004 <saikiran.bavandla@gmail.com>
2020-01-23 14:39:59 +0530

    commit

---
+++ Utils/qrCode.py
@@ -0,0 +1,36 @@
+import cv2
+import qrcode
+
+""""""""""""""" Generate QR Code """""""""""""""
+
+data = "https://www.thepythoncode.com"
+
+filename = "site.png"
+
+img = qrcode.make(data)
+
+img.save(filename)
+
+""""""""""""""" Reading QR Code """""""""""""""
+
+# read the QRCODE image
+img = cv2.imread("site.png")
+
+detector = cv2.QRCodeDetector()
+
+data, bbox, straight_qrcode = detector.detectAndDecode(img)
+# if there is a QR code
+if bbox is not None:
+    print(f"QRCode data:\n{data}")
+    # display the image with lines
+    # length of bounding box
+    n_lines = len(bbox)
+    for i in range(n_lines):
+        # draw all lines
+        point1 = tuple(bbox[i][0])
+        point2 = tuple(bbox[(i+1) % n_lines][0])
+        cv2.line(img, point1, point2, color=(255, 0, 0), thickness=2)
+# display the result
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
