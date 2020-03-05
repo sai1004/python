9b3f454ceaaa6ae3648ad994caf86e03f0c13662
saikiran <35389534+sai1004@users.noreply.github.com>
2020-03-05 22:56:41 +0530

    Create staticInstance.py

---
+++ 01.chapters/chapter3/oops/part-1/staticInstance.py
@@ -0,0 +1,17 @@
+# static and instance methods:
+
+class Employee:
+
+    def employeeDetails(self):
+        self.name = "Ben"
+        
+    @staticmethod # by implemnting static method, enabling to dont throw error on not passing self parm in method
+    def welcomeMessage():
+        print("welcome to our Organisation! ")
+        
+ 
+ 
+employee = Employee()
+employee.employeeDetails()
+print(employee.name)
+employee.welcomeMessage()
