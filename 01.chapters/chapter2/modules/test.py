9ab5c0986a162684dd713721c593a6fbb9397464
sai1004 <saikiran.bavandla@gmail.com>
2020-03-05 17:02:48 +0530

    commit

--- 01.chapters/chapter2/modules/test.py
+++ 01.chapters/chapter2/modules/test.py
@@ -2,4 +2,10 @@
 
 import prime  # prime is a file name exists in a same dir
 
+# import prime as pr # can import this way too
+
+#  from prime import primesTo # can import this way too
+
 prime.primesTo(100)
+
+print(dir(prime))  # returns all the functions in the module
---
+++ 01.chapters/chapter3/regx.py
@@ -0,0 +1,10 @@
+# Regular Expressions
+
+import re
+
+match = re.search('pattern', 'searching pattern in text')
+
+print(match.re.pattern)
+print(match.string)
+print(match.start())  # returns 10 starts pattern position p index position is at 10
+print(match.end()) # returns 17 
