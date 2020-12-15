0d2cc32aaa1a0ee6661afd30ac081a40377d2ae4
sai1004 <saikiran.bavandla@gmail.com>
2020-12-15 23:28:20 +0530

    commit

---
+++ 02.Assignments/creatingEmptyObjs.py
@@ -0,0 +1,23 @@
+# creating empty objects
+
+
+i1 = 0
+i2 = int()
+
+
+str1 = ""
+
+str2 = str()
+
+
+obj1 = {}
+
+obj2 = dict()
+
+
+list1 = []
+
+list2 = list()
+
+
+set1 = set()
---
+++ 02.Assignments/sysModule.py
@@ -0,0 +1,7 @@
+import sys
+
+platform = sys.platform
+
+version_info = sys.version_info
+
+print(platform, version_info)
